9e9023187bb7d6ecd2148e8300546ebf42641996
Akash Biswas <akashbiswas.271999@gmail.com>
2026-07-02 12:51:28 +0530

    Removed proto-gen code from git.

--- services/analysis-agent/main.py
+++ services/analysis-agent/main.py
@@ -1,10 +1,14 @@
-from agent.analyzer import AnalyzerAgentState, analyzer_agent
 
-initial_state: AnalyzerAgentState = {
-    "loop_times": 3,
-    "data_source_id": "acsbBCJKSkjbASKJBk",
-    "messages": [],
-}
-state = analyzer_agent.invoke(initial_state)
+# initial_state: AnalyzerAgentState = {
+#     "loop_times": 3,
+#     "data_source_id": "acsbBCJKSkjbASKJBk",
+#     "messages": [],
+# }
+# state = analyzer_agent.invoke(initial_state)
 
-print(state)
+# print(state)
+
+
+from tools.data import 
+
+
